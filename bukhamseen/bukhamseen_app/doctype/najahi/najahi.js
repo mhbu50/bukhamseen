00e0aa301353536ab97b3cd02243918f640fb9a1
mhbu50 <mhbu50@gmail.com>
2018-06-26 17:54:39 +0300

    add js to retrive date from profile

--- bukhamseen/bukhamseen_app/doctype/najahi/najahi.js
+++ bukhamseen/bukhamseen_app/doctype/najahi/najahi.js
@@ -2,7 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Najahi', {
-	refresh: function(frm) {
+	onload: function(frm) {
+
+		frm.set_value("member",frappe.session.user);
+
+
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "User",
+				name: frappe.session.user
+			},
+			callback: function(data) {
+				if (data.message) {
+					frm.set_value("full_name",data.message.full_name);
+
+
+					frm.refresh_field("member");
+					frm.refresh_field("full_name");
+					 console.log("data",data);
+				}
+			}
+		});
+
+
 
 	}
 });
